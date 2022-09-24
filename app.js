50fc5e3415920ce4a6e3414c16223919582ba86c
Joel Lehtonen <joel.lehtonen@iki.fi>
2022-09-24 17:40:37 +0300

    Matrixification, part 1

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const { createApp } = Vue
 createApp({
     data() {
 	return {
-	    lab: { loading: true }
+	    lab: { loading: true },
+	    jsdebug: "Huolletaan widgettiä, v1"
 	}
     },
     created() {
@@ -14,12 +15,33 @@ createApp({
 	setInterval(function () {
 	    that.fetchData.apply(that)
 	}, 60000)
+	// Start listening to Matrix client events
+	window.addEventListener("message", this.matrixIncoming, false);
     },
     methods: {
 	async fetchData() {
 	    this.lab = await (await fetch(API_URL)).json()
 	    this.lab.loading = false
 	},
+	matrixIncoming(event) {
+	    if (event.origin !== "vector://vector") {
+		console.log("Visitor widget got unsolisited event", event)
+		return
+	    }
+
+	    const handler = {
+		capabilities : this.tellCapabilities,
+	    }[event.data.action];	    
+	    if (handler === undefined) {
+		console.log("Visitor widget cannot process "+event.action, event)
+		return
+	    }
+
+	    handler(event)
+	},
+	tellCapabilities(event) {
+	    console.log("Ominaisuuksia kerrotaan", event)
+	}
     }
 }).mount('#app')
 
